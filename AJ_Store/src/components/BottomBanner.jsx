0086ea4cababaa01ea4199f06b76331dcde4db66
Arjunbhaliya <167542001+Arjunbhaliya@users.noreply.github.com>
2025-06-15 16:08:45 -0500

    new arjun3

--- AJ_Store/src/components/BottomBanner.jsx
+++ AJ_Store/src/components/BottomBanner.jsx
@@ -5,6 +5,8 @@ export const BottomBanner = () => {
   return (
     <div className='relative mt-24'>
         <img src={assets.logo} alt='banner' className='w-full hidden md:block'></img>
+        <img src={assets.logo} alt='banner' className='w-full hidden md:block'></img>
+        <img src={assets.logo} alt='banner' className='w-full hidden md:block'></img>
         <img src={assets.logo} alt='banner' className='w-full  md:hidden'></img>
 
     </div>
