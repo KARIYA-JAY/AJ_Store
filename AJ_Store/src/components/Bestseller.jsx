ae72a62d96028f6e251eb2cfcac29dda72c4a2d2
Arjunbhaliya <167542001+Arjunbhaliya@users.noreply.github.com>
2025-06-16 16:28:15 -0500

    bestseller add

--- AJ_Store/src/components/Bestseller.jsx
+++ AJ_Store/src/components/Bestseller.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Productcart from './Productcart'
+import { useAppContext } from '../context/Appcontext'
 
 export default function Bestseller() {
+  const {products} = useAppContext()
+
   return (
     <div className="mt-16">
       <p className="text-2xl md:text-3xl font-medium">Best Sellers</p>
-      <div>
-        <Productcart/>
+      <div className="grid grid-cols-2 sm:grid-clos-3 md:grid-cols-4  gap-3 md:gap-6  mt-6">
+        {products.filter((product)=>product.inStock).slice(0,8).map((product,index)=>(
+          <Productcart key={index} product={product}/>
+        ))}
+        
       </div>
     </div>
   )
